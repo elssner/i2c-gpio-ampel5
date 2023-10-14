cdfd2fb7d225bb890ef6548c2e1cc3cea0902b4b
GTA Calliope <73078489+elssner@users.noreply.github.com>
2023-10-14 22:41:20 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,9 +3,22 @@ input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
 })
 function Ampelsteuerung () {
     basic.pause(500)
-    GPIOAmpel(1, 1, 1, 1, 1)
+    GPIOAmpel(0, 0, 1, 1, 0)
+    basic.pause(1000)
+    GPIOAmpel(0, 1, 0, 1, 0)
+    basic.pause(2000)
+    GPIOAmpel(1, 0, 0, 1, 0)
+    basic.pause(2000)
+    GPIOAmpel(1, 0, 0, 0, 1)
 }
 function GPIOAmpel (Rot: number, Gelb: number, Grün: number, Rot2: number, Grün2: number) {
+    Byte = 0
+    Byte += Rot * 2 ** 0
+    Byte += Gelb * 2 ** 1
+    Byte += Grün * 2 ** 2
+    Byte += Rot2 * 2 ** 3
+    Byte += Grün2 * 2 ** 4
+    qwiicgpio.writeOUTPUT_PORT(qwiicgpio.qwiicgpio_eADDR(qwiicgpio.eADDR.GPIO_x27), Byte)
     if (Rot == 1) {
         basic.setLedColor(0xff0000)
     } else if (Gelb == 1) {
@@ -22,8 +35,6 @@ function GPIOAmpel (Rot: number, Gelb: number, Grün: number, Rot2: number, Grü
     } else {
         basic.clearScreen()
     }
-    Byte = 0
-    Byte += Rot * 2 ** 0
 }
 let Byte = 0
 qwiicgpio.beimStart(qwiicgpio.qwiicgpio_eADDR(qwiicgpio.eADDR.GPIO_x27))
@@ -38,3 +49,9 @@ qwiicgpio.eIO.OUT,
 qwiicgpio.eIO.OUT,
 qwiicgpio.eIO.OUT
 )
+loops.everyInterval(1000, function () {
+    if (qwiicgpio.readINPUT_PORT(qwiicgpio.qwiicgpio_eADDR(qwiicgpio.eADDR.GPIO_x27)) == 64) {
+        basic.setLedColor(0x0000ff)
+        Ampelsteuerung()
+    }
+})
